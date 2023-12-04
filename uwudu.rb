ea37dac1c1337bab22af6eb58a8efd18bdd926f7
eshanp32 <72509611+eshanp32@users.noreply.github.com>
2023-12-04 17:45:10 +0530

    Update formula

--- uwudu.rb
+++ uwudu.rb
@@ -1,7 +1,7 @@
 # (U w U)_/××××××××××××××××××××  Brew tap formula  ××××××××××××××××××××\_(U w U)
 
 class Uwudu < Formula
-  desc "your cli todo list manager"
+  desc "Your CLI todo list manager"
   homepage "https://github.com/eshanp32/uwudu"
   url "https://github.com/eshanp32/uwudu/archive/refs/tags/v0.1.0.tar.gz"
   sha256 "2d674e9153a63ebfd845fc9a142bfffe32d2f8282f232a68139033647ee338b1"
@@ -10,11 +10,16 @@ class Uwudu < Formula
   depends_on "python@3.9"
 
   def install
-    system "python3", *Language::Python.setup_install_args(prefix)
-    bin.install "bin/uwudu"
+    venv = virtualenv_create(libexec, "python3")
+    venv.pip_install resources
+
+    system "python3", *Language::Python.setup_install_args(libexec)
+
+    bin.install_symlink libexec/"bin/uwudu"
   end
 
   test do
     assert_match "uwudu", shell_output("#{bin}/uwudu --version")
   end
 end
+
